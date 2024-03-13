dd42cfc4303e01967582c29c9896a59e53508c65
vinayakhiwse <vinayak@empiricinfotech.com>
2024-03-13 10:57:43 +0530

    added-changes

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Model({ handleModel , transferToken}) {
+function Model({ handleModel, transferToken }) {
   return (
     <>
       <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center bg-black bg-opacity-30 z-20">
@@ -35,3 +35,49 @@ function Model({ handleModel , transferToken}) {
 }
 
 export default Model;
+
+{
+  /* <div className="mb-5">
+          <label
+            htmlFor="large-input"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Amount
+          </label>
+          <input
+            type="number"
+            id="large-input"
+            name="Amount"
+            placeholder="Enter Amount"
+            value={data.Amount}
+            onChange={handleChange}
+            className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+          />
+        </div> */
+}
+{
+  /* <label
+htmlFor="tokenoption"
+className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+>
+Select an option
+</label> */
+}
+{
+  /*       
+        <select
+          id="tokenoption"
+          name="Token"
+          value={data.Token}
+          onChange={handleChange}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          required
+        >
+          <option defaultValue="Select Token">Select Token</option>
+          <option value="b4b">b4b</option>
+          <option value="b4re">b4re</option>
+          <option value="b4rc">b4rc</option>
+          <option value="matic">matic</option>
+        </select> */
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -126,10 +126,30 @@ export default function Home() {
   return (
     <main className="mt-24">
       <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
+        <label
+          htmlFor="Token"
+          className="block mb-1 mt-3 ps-1 text-base font-medium text-gray-900"
+        >
+          Select an Token
+        </label>
+        <select
+          id="Token"
+          name="Token"
+          value={data.Token}
+          onChange={handleChange}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          required
+        >
+          <option selected>Select Token</option>
+          <option value="b4b">b4b</option>
+          <option value="b4re">b4re</option>
+          <option value="b4rc">b4rc</option>
+          <option value="matic">matic</option>
+        </select>
         <div className="mb-5">
           <label
             htmlFor="large-input"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            className="block mb-1 mt-3 ps-1 text-base font-medium text-gray-900"
           >
             From Address
           </label>
@@ -147,7 +167,7 @@ export default function Home() {
         <div className="mb-5">
           <label
             htmlFor="large-input"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            className="block mb-1 mt-3 ps-1 text-base font-medium text-gray-900"
           >
             From Address Private Key
           </label>
@@ -165,7 +185,7 @@ export default function Home() {
         <div className="mb-5">
           <label
             htmlFor="large-input"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            className="block mb-1 mt-3 ps-1 text-base font-medium text-gray-900"
           >
             To Address
           </label>
@@ -180,13 +200,13 @@ export default function Home() {
             required
           />
         </div>
-        <div className="mb-5">
-          <label
-            htmlFor="large-input"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Amount
-          </label>
+        <label
+          htmlFor="search"
+          className="block mb-1 mt-3 ps-1 text-base font-medium text-gray-900"
+        >
+          Enter Amount
+        </label>
+        <div className="relative">
           <input
             type="number"
             id="large-input"
@@ -194,30 +214,16 @@ export default function Home() {
             placeholder="Enter Amount"
             value={data.Amount}
             onChange={handleChange}
-            className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="block w-full p-4 ps-4 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             required
           />
+          <button
+            type="button"
+            className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Max Token
+          </button>
         </div>
-        <label
-          htmlFor="tokenoption"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Select an option
-        </label>
-        <select
-          id="tokenoption"
-          name="Token"
-          value={data.Token}
-          onChange={handleChange}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          required
-        >
-          <option defaultValue="Select Token">Select Token</option>
-          <option value="b4b">b4b</option>
-          <option value="b4re">b4re</option>
-          <option value="b4rc">b4rc</option>
-          <option value="matic">matic</option>
-        </select>
         <div>{errorMessage && errorMessage}</div>
         <div className="w-full flex items-center justify-center mt-4">
           {loading ? (
